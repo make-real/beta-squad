279e28fd27e58f1701496772acb8574e65c11eed
taiseen <taiseen.cse@gmail.com>
2022-06-22 17:20:38 +0600

    Add Cade | Modal Input ==> OK

--- src/components/List.jsx
+++ src/components/List.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import { RiAddCircleFill } from 'react-icons/ri';
+import { useState } from 'react'
+import { AddCard } from '.';
+
 
 const List = () => {
+
+    const [addCard, setAddCard] = useState(true);
+
     return (
-        <div>List</div>
+        <section className='p-3 bg-gray-50 h-[92vh]'>
+
+
+
+
+
+            {/* 🟨🟨🟨 Position Fixed at the bottom */}
+            <div className='fixed bottom-0 left-0 w-full p-4 bg-gray-100'>
+
+                <div onClick={() => setAddCard(true)}
+                    className='flex gap-3 items-center border border-dashed border-black p-3 rounded bg-white text-gray-500 cursor-pointer duration-300 hover:bg-gray-200'
+                >
+                    <RiAddCircleFill className='text-2xl' />
+                    <h1>ADD A CARD</h1>
+                </div>
+
+                {
+                    addCard && <AddCard setAddCard={setAddCard} />
+                }
+            </div>
+
+        </section>
     )
 }
 
--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 export { default as Calender } from './Calender';
 export { default as Timeline } from './Timeline';
+export { default as AddCard } from './AddCard';
 export { default as NavBar } from './NavBar';
 export { default as Board } from './Board';
 export { default as Chat } from './Chat';
