37839fb5124281b79f41754dc6b5274b1687686f
MousumiMitu <mousumitu@gmail.com>
2022-07-02 17:55:17 +0600

    chat fixed

--- src/components/Chat/Chat.jsx
+++ src/components/Chat/Chat.jsx
@@ -4,19 +4,20 @@ import MessageBox from "./MessageBox";
 import TextMessage from "./TextMessage";
 
 const Chat = () => {
-
   const { margin } = useStyleContext();
 
   return (
-    <div className={`${margin ? 'ml-[325px]' : 'ml-[50px]'} pt-[90px] duration-200 px-4 chat-height`}>
-
-      <div className="fixed bottom-0 w-11/12">
-        <TextMessage />
-        <div className="w-[95%]">
-          <MessageBox />
-        </div>
+    <div
+      className={`${
+        margin ? "ml-[325px]" : "ml-[50px]"
+      } fixed bottom-0 left-0 right-0 pt-[90px] duration-200 px-4`}
+    >
+      {/* <div className="fixed bottom-0   w-11/12"> */}
+      <TextMessage />
+      <div className="w-[95%]">
+        <MessageBox />
       </div>
-
+      {/* </div> */}
     </div>
   );
 };
