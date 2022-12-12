f6bf0df482d9c1c6b0e3f4bbd1208cc5fffe3f9d
iamajraj <akmalraj07@gmail.com>
2022-12-13 00:42:34 +0600

    chat added to chat screen

--- src/components/Chat/Single/Chat.jsx
+++ src/components/Chat/Single/Chat.jsx
@@ -7,9 +7,11 @@ import { useDispatch } from "react-redux";
 import PrivateTextMessage from "../PrivateTextMessage";
 import PrivateMessageBox from "../PrivateMessageBox";
 import BackArrowIcon from "../../../assets/back_arrow.svg";
+import SearchIcon from "../../../assets/search.svg";
 
 const SingleChat = () => {
     const { participantID } = useParams();
+    const [selectedTab, setSelectedTab] = useState("messages");
     const [messageToRespond, setMessageToRespond] = useState();
     const currentWorkspace = useSelector(
         (state) => state.workspace.currentWorkspace
@@ -39,6 +41,16 @@ const SingleChat = () => {
         }
     };
 
+    const TabsScreen = {
+        messages: <SingleChatScreen />,
+        board: <p>Board</p>,
+    };
+
+    const TabsName = {
+        messages: "Messages",
+        board: "Board",
+    };
+
     return (
         <div className="relative pt-[45px] px-[63px] pb-[60px] bg-[#F9F9FF] h-full flex flex-col">
             <div className="flex items-center">
@@ -51,31 +63,110 @@ const SingleChat = () => {
                 <p className="mr-[10px] text-[#00000020] text-[15px] font-medium">
                     /
                 </p>
-                <p className="text-[#031124] text-[15px] font-medium"></p>
+                <p className="text-[#031124] text-[15px] font-medium">Happy</p>
             </div>
-            <div className="mt-[40px] w-full h-full bg-white rounded-[16px] px-[60px] pt-[50px] pb-[36px]">
-                <div className="w-full mt-[38px]">
-                    <div className={`bg-[#ECECEC] pb-5 rounded-lg`}>
-                        <div
-                            style={{
-                                height: `calc(100vh - ${
-                                    messageToRespond ? 245 : 145
-                                }px)`,
-                            }}
-                            className={`overflow-y-auto hide-scrollbar overflow-x-hidden border-b-[0.5px] pt-5 customScroll`}
-                        >
-                            <PrivateTextMessage
-                                messageToRespond={messageToRespond}
-                                setMessageToRespond={setMessageToRespond}
+            <div className="mt-[40px] w-full h-full bg-white rounded-[16px] px-[60px] pt-[50px] pb-[36px] flex flex-col">
+                <div className="flex items-center justify-between">
+                    <div className="flex items-center gap-[11px]">
+                        <div className="relative">
+                            <img
+                                src="https://images.unsplash.com/photo-1599834562135-b6fc90e642ca?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTJ8fGJlYXJkZWR8ZW58MHx8MHx8&w=1000&q=80"
+                                alt=""
+                                className="w-[27px] h-[27px] rounded-full border-[2px] border-[#6576FF] object-cover"
+                            />
+                            <div className="absolute w-[12px] h-[12px] rounded-full bg-[#54CC7C] bottom-0 right-[-4px]"></div>
+                        </div>
+                        <h2 className="text-[20px] text-[#424D5B] font-semibold mr-[9px]">
+                            Happy
+                        </h2>
+                    </div>
+
+                    <div className="flex items-center">
+                        <div className="flex items-center gap-[12px]">
+                            <img src={SearchIcon} alt="search" className="" />
+                            <input
+                                type="text"
+                                placeholder="Search here"
+                                className=" placeholder:text-[#99A6B9] border-none outline-none"
                             />
                         </div>
-                        <PrivateMessageBox
-                            messageToRespond={messageToRespond}
-                            setMessageToRespond={setMessageToRespond}
-                        />
                     </div>
                 </div>
+                <div className="mt-[38px]">
+                    <div className="flex items-center gap-[45px]">
+                        {Object.keys(TabsName).map((value) => {
+                            return (
+                                <h2
+                                    onClick={() => setSelectedTab(value)}
+                                    className={`${
+                                        selectedTab === value
+                                            ? "border-b-2 border-b-[#6576FF] text-[#031124]"
+                                            : "text-[#818892]"
+                                    } text-[19px] font-medium  pb-[10px] cursor-pointer`}
+                                >
+                                    {TabsName[value]}
+                                </h2>
+                            );
+                        })}
+                    </div>
+                </div>
+                <div className="w-full mt-[40px] h-full">
+                    {TabsScreen[selectedTab]}
+                </div>
             </div>
+        </div>
+    );
+};
+
+// Single Chat work should done here.........
+const SingleChatScreen = ({ participantID }) => {
+    const [messageToRespond, setMessageToRespond] = useState();
+
+    const dispatch = useDispatch();
+
+    const { selectedWorkspace } = useSelector((state) => state.workspace);
+
+    useEffect(() => {
+        getMessages();
+    }, [participantID, selectedWorkspace]);
+
+    const getMessages = async () => {
+        try {
+            console.log("Sending...");
+            const { data } = await get_single_messages(
+                selectedWorkspace,
+                participantID
+            );
+
+            console.log(data.message);
+
+            dispatch(addBulkMessagePrivate(data.messages.reverse()));
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    return (
+        <div className={`bg-[#ECECEC] rounded-lg h-full`}>
+            <div
+                // Previous
+                // style={{
+                //     height: `calc(100vh - ${messageToRespond ? 245 : 145}px)`,
+                // }}
+                // now
+                style={{
+                    height: `calc(100% - ${messageToRespond ? 170 : 70}px)`,
+                }}
+                className={`overflow-y-auto hide-scrollbar overflow-x-hidden border-b-[0.5px] pt-5 customScroll h-full`}
+            >
+                <PrivateTextMessage
+                    messageToRespond={messageToRespond}
+                    setMessageToRespond={setMessageToRespond}
+                />
+            </div>
+            <PrivateMessageBox
+                messageToRespond={messageToRespond}
+                setMessageToRespond={setMessageToRespond}
+            />
         </div>
     );
 };
