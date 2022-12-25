2ca135359aaa8552e83d6fb60112d98b59b7d5a3
iamajraj <akmalraj07@gmail.com>
2022-12-25 23:13:31 +0600

    local storage set for stepFinished field if workspace exists

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -47,6 +47,7 @@ const Home = () => {
         if (shouldChangeRoute) {
             navigate("/settings/manage-workspace");
         } else {
+            localStorage.setItem("stepFinished", true);
             navigate("/projects");
         }
     }, [shouldChangeRoute, loading]);
