f582843166a6e9471dec98f2361ad63b026a80a0
sagorsohel <khaman2645@gmail.com>
2023-09-02 02:04:33 +0600

    mobile welcome page

--- src/components/LoginRegistration/Welcome.jsx
+++ src/components/LoginRegistration/Welcome.jsx
@@ -1,9 +1,12 @@
-
-
+import CatImage from '../../assets/CatImage.svg'
+ 
 const Welcome = () => {
     return (
-        <div>
-            sohel
+        <div className=" w-[340px] space-y-3  flex flex-col items-center justify-center
+         mx-auto text-center py-auto h-[90vh]">
+          <img src={CatImage} alt="" />
+          <h1 className='text-xl pt-4 text-center font-medium leading-6'>Welcome!</h1>
+          <p className='text-center mx-10 font-light leading-6 text-[15px] w-[340px] text-[#031124]'>Thanks for signing up. Our Mobile App will be Available soon. Until then please use Squad in a desktop or laptop screen.</p>
         </div>
     );
 };
