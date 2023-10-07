3d41bd21e99102067136d620e6226ddaded498a2
Mahbub Rahman <mahbub@makereal.io>
2023-10-07 12:33:57 +0600

    Revert "Dev sojib"

--- src/components/Navs/TopNavbar.jsx
+++ src/components/Navs/TopNavbar.jsx
@@ -443,9 +443,9 @@ const LoggedInTopNav = () => {
 const AllNotification = ({ notifications }) => {
   return (
     <div className="flex flex-col gap-[4px] overflow-y-scroll h-full">
-      {notifications?.map((notification, i) => {
+      {notifications?.map((notification) => {
         return (
-          <div key={i} className="relative w-full pl-[16px] pr-[36px] py-[13px] flex items-center justify-between bg-[#C4FFF5] rounded-[10px]">
+          <div className="relative w-full pl-[16px] pr-[36px] py-[13px] flex items-center justify-between bg-[#C4FFF5] rounded-[10px]">
             <div className="flex items-center gap-[17px]">
               <div className="w-[50px] h-[50px] flex items-center justify-center bg-white rounded-full shrink-0">
                 <BellIcon style={{ fill: "#13E5C0" }} />
@@ -502,9 +502,9 @@ const AllNotification = ({ notifications }) => {
 const UnreadNotification = ({ notifications }) => {
   return (
     <div className="flex flex-col gap-[4px] overflow-y-scroll h-full">
-      {notifications?.map((notification, i) => {
+      {notifications?.map((notification) => {
         return (
-          <div key={i} className="relative w-full pl-[16px] pr-[36px] py-[13px] flex items-center justify-between bg-[#FFEBF2] rounded-[10px]">
+          <div className="relative w-full pl-[16px] pr-[36px] py-[13px] flex items-center justify-between bg-[#FFEBF2] rounded-[10px]">
             <div className="flex items-center gap-[17px]">
               <div className="w-[50px] h-[50px] flex items-center justify-center bg-white rounded-full shrink-0">
                 <BellIcon style={{ fill: "#FB397F" }} />
