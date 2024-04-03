9a3c296581bf2e9c0fdf136eb30f41aad3c94ab8
Borhan <brborhan70@gmail.com>
2024-04-03 07:39:47 +0600

    notice  date show

--- src/components/Navs/TopNavbar.jsx
+++ src/components/Navs/TopNavbar.jsx
@@ -35,6 +35,9 @@ const TopNav = () => {
   );
 };
 
+
+
+
 const NotLoggedInTopNav = () => {
   const location = useLocation();
   const currentPage =
@@ -109,6 +112,7 @@ const LoggedInTopNav = () => {
     navigate("/");
   };
 
+  
   const fetchNotification = async () => {
     try {
       const { data } = await get_notifications(10);
@@ -198,7 +202,32 @@ const LoggedInTopNav = () => {
   }, []);
 
 
+  const calculateTimeDifference = (pastTime) => {
+    const currentTime = new Date();
+    const pastDateTime = new Date(pastTime);
+    const difference = currentTime - pastDateTime;
 
+    const seconds = Math.floor(difference / 1000);
+    const minutes = Math.floor(seconds / 60);
+    const hours = Math.floor(minutes / 60);
+    const days = Math.floor(hours / 24);
+    const years = Math.floor(days / 365);
+
+    if (years > 0) {
+      return `${years} year${years > 1 ? 's' : ''} ago`;
+    } else if (days > 0) {
+      return `${days} day${days > 1 ? 's' : ''} ago`;
+    } else if (hours > 0) {
+      return `${hours} hour${hours > 1 ? 's' : ''} ago`;
+    } else if (minutes > 0) {
+      return `${minutes} minute${minutes > 1 ? 's' : ''} ago`;
+    } else {
+      return `${seconds} second${seconds > 1 ? 's' : ''} ago`;
+    }
+  };
+
+
+console.log(notifications)
   return (
     <>
       <div
@@ -258,7 +287,9 @@ const LoggedInTopNav = () => {
                           <p className="text-[#031124]">
                             {notification?.message}
                           </p>
+                         
                         </div>
+                        <span className="text-[12px] absolute bottom-[2px] px-[3px] right-1">{calculateTimeDifference(notification?.createdAt)}</span>
                       </div>
                     );
                   })}
